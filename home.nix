d8888eef88e9bfd86e77cf560c7924b47dd5682d
Jacob R. <jacobrambarran@gmail.com>
2025-05-14 19:18:11 -0400

    fixing file structure

--- home.nix
+++ home.nix
@@ -395,20 +395,44 @@ programs.ghostty = {
 
     home.file = {
         ".config/helix" = {
-            source = ./helix;
+            source = ./dots/helix;
         };
+        ".config/i3" = {
+            source = ./dots/i3;
+        };
+        ".config/nvim" = {
+            source = ./dots/nvim;
+        };
+        ".config/rofi" = {
+            source = ./dots/rofi;
+        };
+        ".config/picom" = {
+            source = ./dots/picom;
+        };
+        ".config/dunst" = {
+            source = ./dots/dunst;
+        };
+        ".config/zellij" = {
+            source = ./dots/zellij;
+        };
+        ".config/starship" = {
+            source = ./dots/starship;
+        };
+
+
+
     };
-  xdg.configFile."i3".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/i3";
-  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim";
-
-  xdg.configFile."picom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/picom";
-  xdg.configFile."dunst".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/dunst";
-  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/rofi";
-  xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/wezterm/starship.toml";
-  # xdg.configFile."helix".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/helix";
-  xdg.configFile."zellij".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zellij";
-   
-
+  # xdg.configFile."i3".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/i3";
+  # xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim";
+  #
+  # xdg.configFile."picom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/picom";
+  # xdg.configFile."dunst".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/dunst";
+  # xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/rofi";
+  # xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/wezterm/starship.toml";
+  # # xdg.configFile."helix".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/helix";
+  # xdg.configFile."zellij".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zellij";
+  #
+  #
 
 
 
