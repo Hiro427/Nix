aca178b35132b14b90700f5a6e3541cc84dfa616
Jacob R. <jacobrambarran@gmail.com>
2025-07-26 03:43:27 -0400

    idk

--- shared/hm/ghostty.nix
+++ shared/hm/ghostty.nix
@@ -1,21 +1,17 @@
-{
-    config, 
-    pkgs, 
-    ...
-}: 
-{
-programs.ghostty = {
+{ config, pkgs, ... }: {
+  programs.ghostty = {
     enable = true;
     settings = {
-          # font-family = "JetBrainsMono Nerd Font";
-          # font-size = 13;
-          gtk-titlebar = false;
-          alpha-blending = "native";
-          window-padding-x = 10;
-          window-padding-y = 5;
-          window-decoration = "client";
-          window-colorspace = "display-p3";
+      # font-family = "JetBrainsMono Nerd Font";
+      # font-size = 13;
+      gtk-titlebar = false;
+      alpha-blending = "native";
+      window-padding-x = 10;
+      window-padding-y = 5;
+      window-decoration = "client";
+      window-colorspace = "display-p3";
+      gtk-single-instance = true;
 
     };
-    };
+  };
 }
