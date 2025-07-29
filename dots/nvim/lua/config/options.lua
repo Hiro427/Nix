4490f79761c9b962792cdf77ede77bf69467e9ba
Jacob R. <jacobrambarran@gmail.com>
2025-07-29 01:43:29 -0400

    new obsidian plugin and added vaults to github

--- dots/nvim/lua/config/options.lua
+++ dots/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set cursorline")
+vim.cmd("set conceallevel=1")
 vim.opt.showmode = false
 vim.o.number = true
 vim.o.relativenumber = true
