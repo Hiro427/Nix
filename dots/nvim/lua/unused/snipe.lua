3e7e2b43b6519934c4ff1e926c43bdb7b79c209a
Hiro427 <jacobrambarran@gmail.com>
2025-12-10 20:53:04 -0500

    removed files

---
+++ dots/nvim/lua/unused/snipe.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"leath-dub/snipe.nvim",
+		keys = {
+			{
+				"ss",
+				function()
+					require("snipe").open_buffer_menu()
+				end,
+				desc = "Open Snipe buffer menu",
+			},
+		},
+		opts = {
+			ui = { position = "center" },
+			-- vsplit = V, split = H, close = D
+		},
+	},
+}
